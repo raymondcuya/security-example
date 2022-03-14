9e77c1d84d4451871e3a77764700a9e9a76ef057
Raymond Cuya <raymondcuya@gmail.com>
2022-03-14 21:25:57 +1100

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+
+const PORT = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/secret', (req, res) => {
+  return res.send('Your personal secret value is 20!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
